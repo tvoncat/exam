b624f5f2f0de473ddd653275e5f4006e4f5ec6e4
Tanmay <tara22ise@cmrit.ac.in>
2025-06-08 23:17:26 +0530

    first

---
+++ src/main/java/Tanmay/prg_11/App.java
@@ -0,0 +1,13 @@
+package Tanmay.prg_11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
